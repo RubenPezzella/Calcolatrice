573c5f49d355603ae8ca0b1b30467f11e770e974
RubenPezzella <rub.pezz28@gmail.com>
2024-11-18 14:19:28 +0100

    Modifica main

--- Calcolatrice/src/it/beTalent/main/Main.java
+++ Calcolatrice/src/it/beTalent/main/Main.java
@@ -5,16 +5,16 @@ import it.beTalent.calcolatrice.Calcolatrice;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Calcolatrice calc = new Calcolatrice();
 		int a = 5;
 		int b = 6;
 		int c = 7;
-		
+
 		int somma = calc.somma(a, b, c);
-		
-		System.out.println(somma);
+		System.out.println("La somma dei numeri è: " + somma);
 
+		int prodotto = calc.moltiplica(a, b, c);
+		System.out.println("Il prodotto dei numeri è: " + prodotto);
 	}
-
 }
