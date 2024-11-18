b4598bb718650e5e5e41f38d50b662475cdc7f80
RubenPezzella <rub.pezz28@gmail.com>
2024-11-18 12:36:44 +0100

    Prima modifica: moltiplicazione

--- Calcolatrice/src/it/beTalent/calcolatrice/Calcolatrice.java
+++ Calcolatrice/src/it/beTalent/calcolatrice/Calcolatrice.java
@@ -1,6 +1,6 @@
 package it.beTalent.calcolatrice;
 
-public class Calcolatrice {
+public class Calcolatrice{
 	
 	public int somma(int primo, int secondo, int terzo) {
 		int risultato;
@@ -8,4 +8,7 @@ public class Calcolatrice {
 		return risultato;
 	}
 
+	public int moltiplica(int primo, int secondo, int terzo) {
+		return primo * secondo * terzo;
+	}
 }
